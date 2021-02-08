fe6c55bdfcaae706c3f31d6632a283cae408b2e0
Serhii Hupalo <dc.menage@gmail.com>
2021-02-08 03:52:18 +0200

    Add backend XML handler

--- Diary/Program.cs
+++ Diary/Program.cs
@@ -6,7 +6,23 @@ namespace Diary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            XMLWorker diary = new XMLWorker();
+            //Console.WriteLine(diary.CheckUserExist("korsa3"));
+
+            var data = diary.GetUserData("korsa");
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Key);
+                foreach (var records in item)
+                {
+                    Console.WriteLine("\t" + records.Value);
+                }
+            }
+
+            //diary.RegisterUser("kor23dsf54gsa123", "ol123olo");
+            Console.WriteLine(diary.SignUp("korsa", "123qwe"));
+            diary.AddNote(DateTime.Now, "korsa", "olol");
         }
     }
 }
---
+++ Diary/XMLWorker.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Diary
+{
+    class XMLWorker
+    {
+        private readonly string documentName = "DiaryData.xml";
+
+        public XMLWorker()
+        {
+            if (!File.Exists(documentName))
+            {
+                CreateEmptyDiary();
+            }
+        }
+
+        private void CreateEmptyDiary()
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.OmitXmlDeclaration = false;
+            settings.Indent = true;
+            settings.NewLineOnAttributes = true;
+
+            var xmlWriter = XmlWriter.Create(documentName, settings);
+
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("Diary");
+            xmlWriter.WriteEndElement();
+            xmlWriter.Close();
+        }
+
+
+        private bool CheckUserExist(string login)
+        {
+            bool userExist = default;
+
+            var reader = XmlReader.Create(documentName);
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name.Equals("User") && reader.GetAttribute("Login") == login)
+                    {
+                        userExist = true;
+                        break;
+                    }
+                }
+            }
+
+            reader.Close();
+
+            return userExist;
+        }
+
+        public IEnumerable<IGrouping<string, XElement>> GetUserData(string login)
+        {
+            XDocument diary = XDocument.Load(documentName);
+            XElement root = diary.Root;
+
+            var userData =
+                from user in root.Elements()
+                where user.Attribute("Login").Value == login
+                from record in user.Elements()
+                group record by record.Attribute("Date").Value into records
+                orderby records.Key descending
+                select records;
+
+            return userData;
+        }
+
+        public void RegisterUser(string login, string password)
+        {
+            if (CheckUserExist(login))
+            {
+                throw new Exception($"User {login} already exists!");
+            }
+            else
+            {
+                var xmldoc = new XmlDocument();
+                xmldoc.Load(documentName);
+
+                XmlNode root = xmldoc.DocumentElement;
+                XmlNode loginAttr = xmldoc.CreateNode(XmlNodeType.Attribute, "Login", "");                
+                XmlNode passwordAttr = xmldoc.CreateNode(XmlNodeType.Attribute, "Password", "");
+
+                loginAttr.Value = login;
+                passwordAttr.Value = password;
+
+                XmlNode newUser = xmldoc.CreateElement("User");
+                newUser.Attributes.SetNamedItem(loginAttr);
+                newUser.Attributes.SetNamedItem(passwordAttr);
+
+                root.AppendChild(newUser);
+
+                xmldoc.Save(documentName);
+            }
+        }
+
+        public bool SignUp(string login, string password)
+        {
+            bool signed = default;
+            if (!CheckUserExist(login))
+            {
+                throw new Exception($"User {login} don`t exist!");
+            }
+            else
+            {
+                var reader = XmlReader.Create(documentName);
+
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        if (reader.Name.Equals("User") 
+                            && reader.GetAttribute("Login") == login 
+                            && reader.GetAttribute("Password") == password)
+                        {
+                            signed = true;
+                            break;
+                        }
+                    }
+                }
+                reader.Close();
+            }
+            return signed;
+        }
+
+        public void AddNote(DateTime date, string login, string text)
+        {
+            var xmldoc = new XmlDocument();
+            xmldoc.Load(documentName);
+
+            XmlNode root = xmldoc.SelectSingleNode($"//User[@Login=\"{login}\"]");
+            XmlNode dateAttr = xmldoc.CreateNode(XmlNodeType.Attribute, "Date", "");
+
+            dateAttr.Value = date.ToShortDateString();
+
+            XmlNode newNote = xmldoc.CreateElement("p");
+            newNote.Attributes.SetNamedItem(dateAttr);
+            newNote.InnerText = text;
+
+            root.AppendChild(newNote);
+
+            xmldoc.Save(documentName);
+        }
+    }
+}
